989b87fbcc39c89909246c73c214060e728f7608
TahaNamdar <tahanamdar30@gmail.com>
2024-08-05 13:05:20 +0330

    tasting all features

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,90 @@
 import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
 import "ag-grid-community/styles/ag-theme-quartz.css";
-import { useState } from "react";
-import { ColDef } from "ag-grid-community"; // Import the ColDef type
+import { useEffect, useState } from "react";
 import "./App.css";
+import "ag-grid-enterprise";
+import { ColDef, ICellRendererParams } from "ag-grid-community"; // Import ColDef and ICellRendererParams
 
 function App() {
-  const CustomButtonComponent = () => {
-    return <button onClick={() => window.alert("clicked")}>Push Me!</button>;
-  };
+  useEffect(() => {
+    fetch("https://www.ag-grid.com/example-assets/olympic-winners.json")
+      .then((response) => response.json())
+      .then((data) => setRowData(data));
+  }, []);
 
-  const plusButtonComponent = (params: any) => {
-    const value = params.value;
-    const rowIndex = params.node.rowIndex;
-
-    const increment = () => {
-      setRowData((prevRowData) => {
-        const updatedRowData = [...prevRowData];
-        updatedRowData[rowIndex].qty += 1;
-        return updatedRowData;
-      });
+  const SimpleComponent = (p: ICellRendererParams & { buttonText: string }) => {
+    const onAt = () => {
+      console.log(p.value);
     };
 
     return (
-      <div>
-        <button onClick={increment}>+</button>
-        {value}
-        {rowData[rowIndex].qty}
-      </div>
+      <>
+        <button onClick={onAt}>{p.buttonText}</button>
+        <div>{p.value}</div>
+      </>
     );
   };
 
-  const [rowData, setRowData] = useState([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true, qty: 0 },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false, qty: 0 },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false, qty: 0 },
-  ]);
+  const [rowData, setRowData] = useState<any[]>([]);
 
   const [colDefs, setColDefs] = useState<ColDef[]>([
-    { field: "make", headerName: "Make", cellRenderer: plusButtonComponent },
-    { field: "model", headerName: "Model" },
     {
-      field: "price",
-      headerName: "Price",
-      filter: true,
-      editable: true,
-      valueFormatter: (p) => "$" + p.value.toLocaleString(),
-      cellClassRules: {
-        "green-cell": (p) => p.value > 60000,
+      field: "athlete",
+      cellRenderer: SimpleComponent,
+      cellRendererParams: { buttonText: "Click Me" },
+    },
+    {
+      field: "age",
+      cellRenderer: (p: ICellRendererParams) => (
+        <>
+          <b>Age is : {p.value}</b>
+        </>
+      ),
+      filterParams: {
+        debounceMs: 2000,
       },
     },
-    { field: "electric", headerName: "Electric", type: "boolean" },
-    { field: "button", cellRenderer: CustomButtonComponent },
+    { field: "country", pinned: "left" },
+    { field: "year", editable: true, checkboxSelection: true },
+    { field: "date" },
+    { field: "sport", lockPinned: true },
+    {
+      headerName: "Medals",
+      children: [
+        { field: "gold" },
+        { field: "silver" },
+        { field: "bronze", columnGroupShow: "closed" },
+      ],
+    },
+    { field: "total" },
   ]);
 
-  const rowClassRules = {
-    "red-row": (params: any) => params.data.make === "Toyota",
+  const groupDisplayType = "groupRows";
+
+  const defaultColDef = {
+    sortable: true,
+    filter: true,
+    resizable: true,
+    enableRowGroup: true,
+
+    filterParams: {
+      buttons: ["apply", "clear"],
+    },
+    width: 150,
   };
 
   return (
     <div>
       <div className="ag-theme-quartz" style={{ height: 600 }}>
         <AgGridReact
-          rowClassRules={rowClassRules}
           rowData={rowData}
           columnDefs={colDefs}
+          rowGroupPanelShow="always"
+          animateRows={true}
+          defaultColDef={defaultColDef}
+          groupDisplayType={groupDisplayType}
+          noRowsOverlayComponent={"Loading..."}
         />
       </div>
     </div>
